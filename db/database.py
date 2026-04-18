4a7e083e27c08848d59627df7854433396623fc8
simsfreak <45701730+simsfreak@users.noreply.github.com>
2026-04-18 17:18:00 -0400

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,1 +1,19 @@
+from contextlib import contextmanager
+import psycopg
+from config import DATABASE_URL
 
+@contextmanager
+def get_conn():
+    conn = psycopg.connect(DATABASE_URL)
+    try:
+        yield conn
+        conn.commit()
+    finally:
+        conn.close()
+
+
+def run_schema():
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            with open("db/schema.sql", "r", encoding="utf-8") as f:
+                cur.execute(f.read())
